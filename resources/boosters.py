062123be708a96210d94ead557920b2c591d5bbe
Daniel Amaral <danielamaral.f@hotmail.com>
2019-05-24 11:45:29 -0300

    fix bugs

--- resources/boosters.py
+++ resources/boosters.py
@@ -7,15 +7,19 @@ class Booster(object):
         self.ranking = None
         self.is_vip = None
         self.item_ = None
+
+        # box configs
+        self.box = {"status": {"active": True, "secret": 0, "ur": 0, "sr": 0, "r": 0, "n": 0, "c": 0}}
         self.legend = {"Comum": 0, "Normal": 1, "Raro": 2, "Super Raro": 3, "Ultra Raro": 4, "Secret": 5}
         self.rarity = {"Comum": 500, "Normal": 400, "Raro": 300, "Super Raro": 200, "Ultra Raro": 150, "Secret": 100}
+
+        # booster configs
         self.booster_choice = None
         self.booster_bronze = {"Comum": 95, "Normal": 1, "Raro": 1, "Super Raro": 1, "Ultra Raro": 1, "Secret": 1}
         self.booster_silver = {"Comum": 60, "Normal": 36, "Raro": 1, "Super Raro": 1, "Ultra Raro": 1, "Secret": 1}
         self.booster_gold = {"Comum": 50, "Normal": 46, "Raro": 1, "Super Raro": 1, "Ultra Raro": 1, "Secret": 1}
         self.booster_vip = {"Comum": 50, "Normal": 30, "Raro": 15, "Super Raro": 2, "Ultra Raro": 1, "Secret": 1}
         self.booster_secret = {"Comum": 40, "Normal": 30, "Raro": 20, "Super Raro": 8, "Ultra Raro": 2, "Secret": 1}
-        self.box = {"status": {"active": True, "secret": 0, "ur": 0, "sr": 0, "r": 0, "n": 0, "c": 0}}
 
         # contadores de itens
         self.secret = 0
@@ -29,12 +33,12 @@ class Booster(object):
         self.box_count = 0
 
         # Limites dos itens
-        self.l_secret = 1  # 1 item de limite
-        self.l_ur = 2 * len([x for x in self.items.keys() if self.items[x][3] == 4])  # 20 itens
-        self.l_sr = 3 * len([x for x in self.items.keys() if self.items[x][3] == 3])  # 57 itens
-        self.l_r = 6 * len([x for x in self.items.keys() if self.items[x][3] == 2])  # 36 itens
-        self.l_n = 28 * len([x for x in self.items.keys() if self.items[x][3] == 1])  # 308 itens
-        self.l_c = 60 * len([x for x in self.items.keys() if self.items[x][3] == 0])  # 600 itens
+        self.l_secret = 1
+        self.l_ur = 2 * len([x for x in self.items.keys() if self.items[x][3] == 4])
+        self.l_sr = 3 * len([x for x in self.items.keys() if self.items[x][3] == 3])
+        self.l_r = 6 * len([x for x in self.items.keys() if self.items[x][3] == 2])
+        self.l_n = 28 * len([x for x in self.items.keys() if self.items[x][3] == 1])
+        self.l_c = 60 * len([x for x in self.items.keys() if self.items[x][3] == 0])
 
     def reset_counts(self):
         self.box = {"status": {"active": True, "secret": 0, "ur": 0, "sr": 0, "r": 0, "n": 0, "c": 0}}
@@ -46,10 +50,8 @@ class Booster(object):
         self.n = 0
         self.c = 0
 
-    def create_booster(self, ranking, is_vip):
+    def create_booster(self):
         self.reset_counts()
-        self.ranking = ranking
-        self.is_vip = is_vip
         rarity = choice(list(self.rarity.keys()))
         size = self.rarity[rarity]
         self.box['status']['rarity'] = rarity
@@ -128,7 +130,7 @@ class Booster(object):
             self.booster_choice = self.booster_gold
 
         if self.is_vip:
-            self.booster_choice = self.booster_bronze
+            self.booster_choice = self.booster_vip
 
         chance = randint(1, 100)
         if chance == 100:
